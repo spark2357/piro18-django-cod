a7c992b7074699ad90c850582ff61fe45da19582
spark2357 <psbted@naver.com>
2023-01-12 15:57:11 +0900

    update 기능 추가

--- server/apps/posts/urls.py
+++ server/apps/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from server.apps.posts.views import posts_list, posts_retrieve, posts_create, posts_delete
+from server.apps.posts.views import posts_list, posts_retrieve, posts_create, posts_delete, posts_update
 
 urlpatterns = [
     path("", posts_list),
     path("posts/create", posts_create),
     path("posts/<int:pk>", posts_retrieve),
+    path("posts/<int:pk>/update", posts_update),
     path("posts/<int:pk>/delete", posts_delete),
 ]
